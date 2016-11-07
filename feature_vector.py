b8b9bb33d8498b64e2f977a71a2d4a179e358b58
schachte <rschacht@asu.edu>
2016-11-07 13:54:44 -0700

    Init commit

---
+++ feature_vector.py
@@ -0,0 +1,16 @@
+'''
+******************************
+Criminology Data Set         *
+Feature Vector Generation    *
+******************************
+
+@Authors
+    * Ryan Schachte
+    * Fatima Naveed
+    * Rob Stevens
+    * Trevor Richardson
+
+@Purpose
+    * Filters the input CSV file to generate an output feature vector CSV
+    * Stores data in external cloud mongoDB for easy filtering
+'''
