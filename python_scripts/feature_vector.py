9906293dd8bd5cb9022813582740e19a9fe82b41
schachte <rschacht@asu.edu>
2016-11-19 18:16:28 -0700

    updated feature_vector.py

--- python_scripts/feature_vector.py
+++ python_scripts/feature_vector.py
@@ -32,41 +32,41 @@ class Feature_Vector:
         '''
         STRUCTURE FOR THE FEATURE VECTOR
 
-        1) Xbin
-        2) Ybin
-        3) Week
-        4) Current week 2016
-        5) Current week 2015
-        6) Current week 2014
-        7) Current week 2013
-        8) Current week 2012
-        9) Past month 2016
-        10) Past month 2015
-        11) Past month 2014
-        12) Past month 2013
-        13) Past month 2012
-        14) Past year 2016
-        15) Past year 2015
-        16) Past year 2014
-        17) Past year 2013
-        18) Past year 2012
-        19) All week = All time
-        20) All month
-        21) All year
+        0) Xbin
+        1) Ybin
+        2) Week
+        3) Current week 2016
+        4) Current week 2015
+        5) Current week 2014
+        6) Current week 2013
+        7) Current week 2012
+        8) Past month 2016
+        9) Past month 2015
+        10) Past month 2014
+        11) Past month 2013
+        12) Past month 2012
+        13) Past year 2016
+        14) Past year 2015
+        15) Past year 2014
+        16) Past year 2013
+        17) Past year 2012
+        18) All week = All time
+        19) All month
+        20) All year
 
         Radii 1
 
-        Features 22 - 39
+        Features 21 - 38
         In same order starting with current week 2016
 
         Radii 2
 
-        Features 40 - 57
+        Features 39 - 56
         same order as above
 
         Radii 3
 
-        Features 58 - 75
+        Features 57 - 74
         same order as above
 
         '''
@@ -119,7 +119,7 @@ class Feature_Vector:
                 wk_no = lookup_dict[current_date_string][0]
                 yr_no = lookup_dict[current_date_string][1]
 
-                sum_total_call = sum(row[3:])
+                sum_total_call = row[7]
 
                 self.update_week(row[1], row[2], wk_no, yr_no, sum_total_call, 3)
                 self.update_month(row[1], row[2], wk_no, yr_no, sum_total_call, 8)
@@ -132,15 +132,12 @@ class Feature_Vector:
         self.calc_radii()
 
     def calc_radii(self):
-        count = 0
         for x in range(0, 359):
             print("%.4f COMPLETE"%((float(x)/359)*100))
             for y in range(0, 359):
                 for week in range(0, 53):
                     #calc Radius for that xbin ybin
                     self.radius_calculation(x,y,week,3)
-                    count += 1
-
 
     def radius_calculation(self, xbin, ybin, wk, radii): # I am counting myself into radius calculations -- make sure take out 0,0 case for that case
         for i in range(0,18):
@@ -148,33 +145,47 @@ class Feature_Vector:
                 for y_range in range(0,current_radius+1):
                     for x_range in range(0,current_radius+1):
                         offset = 3 + 18*current_radius
+
+                        #Taken care of (0, 0)
                         if (x_range == 0 and y_range == 0):
                             self.feature_dict[(xbin, ybin, wk)][i+offset] += self.feature_dict[(xbin+x_range, ybin+y_range, wk)][i + 3]
                         else:
+                            #Taken care of (0, 1), (1, 1), (1, 0)
                             if (xbin + x_range >= 0 and ybin + y_range >= 0 and xbin + x_range < 359 and ybin+y_range < 359): #only update if it wont give index out of bounds
                                 self.feature_dict[(xbin, ybin, wk)][i + offset] += self.feature_dict[(xbin+x_range, ybin+y_range, wk)][i + 3]
 
+                            #Taken care of (0, -1), (-1, -1), (-1, 0)
                             if(xbin - x_range >= 0 and ybin - y_range >= 0 and xbin - x_range < 359 and ybin - y_range < 359): #only update if it wont give index out of bounds
                                 self.feature_dict[(xbin, ybin, wk)][i + offset] += self.feature_dict[(xbin - x_range, ybin - y_range, wk)][i + 3]
 
 
+                            if (not x_range == 0 and not y_range == 0):
+
+                                #Taken care of (1, -1)
+                                if (xbin + x_range >= 0 and ybin + y_range >= 0 and xbin + x_range < 359 and ybin+y_range < 359): #only update if it wont give index out of bounds
+                                    self.feature_dict[(xbin, ybin, wk)][i + offset] += self.feature_dict[(xbin+x_range, ybin-y_range, wk)][i + 3]
+
+                                #Taken care of (-1, 1)
+                                if(xbin - x_range >= 0 and ybin - y_range >= 0 and xbin - x_range < 359 and ybin - y_range < 359): #only update if it wont give index out of bounds
+                                    self.feature_dict[(xbin, ybin, wk)][i + offset] += self.feature_dict[(xbin - x_range, ybin + y_range, wk)][i + 3]
+
     def update_sum_features(self):
-        week_index = 19
-        month_index = 20
-        year_index = 21
+        week_index = 18
+        month_index = 19
+        year_index = 20
 
         for x in range(0, 359):
             for y in range(0, 359):
                 for week in range(0, 53):
-                    self.feature_dict[(x, y, week)][18] = (self.feature_dict[(x, y, week)][3] + self.feature_dict[(x, y, week)][4]+
+                    self.feature_dict[(x, y, week)][week_index] = (self.feature_dict[(x, y, week)][3] + self.feature_dict[(x, y, week)][4]+
                                                         self.feature_dict[(x, y, week)][5]+self.feature_dict[(x, y, week)][6]+
                                                         self.feature_dict[(x, y, week)][7])
 
-                    self.feature_dict[(x, y, week)][19] = (self.feature_dict[(x, y, week)][8] + self.feature_dict[(x, y, week)][9] +
+                    self.feature_dict[(x, y, week)][month_index] = (self.feature_dict[(x, y, week)][8] + self.feature_dict[(x, y, week)][9] +
                                                         self.feature_dict[(x, y, week)][10] + self.feature_dict[(x, y, week)][11] +
                                                         self.feature_dict[(x, y, week)][12])
 
-                    self.feature_dict[(x, y, week)][20] = (self.feature_dict[(x, y, week)][13] + self.feature_dict[(x, y, week)][14]+
+                    self.feature_dict[(x, y, week)][year_index] = (self.feature_dict[(x, y, week)][13] + self.feature_dict[(x, y, week)][14]+
                                                         self.feature_dict[(x, y, week)][15]+self.feature_dict[(x, y, week)][16]+
                                                         self.feature_dict[(x, y, week)][17])
 
@@ -182,13 +193,13 @@ class Feature_Vector:
     def update_week(self, xbin, ybin, wk_no, yr, total_call, feature_location):
         '''Update the total call sum for each respective week'''
 
-        if(yr == '2016'):
+        if(yr == '16'):
             self.feature_dict[(xbin, ybin, wk_no)][feature_location] += total_call
-        elif(yr == '2015'):
+        elif(yr == '15'):
             self.feature_dict[(xbin, ybin, wk_no)][feature_location+1] += total_call
-        elif(yr == '2014'):
+        elif(yr == '14'):
             self.feature_dict[(xbin, ybin, wk_no)][feature_location+2] += total_call
-        elif(yr == '2013'):
+        elif(yr == '13'):
             self.feature_dict[(xbin, ybin, wk_no)][feature_location+3] += total_call
         else:
             self.feature_dict[(xbin, ybin, wk_no)][feature_location+4] += total_call
@@ -196,31 +207,25 @@ class Feature_Vector:
     def get_weeks_to_update(self,wk_no, weeks_forward):
         week_vector = []
         for x in range(1,weeks_forward+1): #generates an array with the weeks we will update
-            appender = wk_no + x
+            appender = [(wk_no + x)%53, (wk_no + x)/53]
             week_vector.append(appender)
-
-        for index in range(0, len(week_vector)): #makes sure we deal with the case we are updating weeks 1-4 for week 52 data
-            if week_vector[index] > 52:
-                week_vector[index] -= 53
-
 
         return week_vector
 
-
     def update_month(self, xbin, ybin, wk_no, yr, total_call, feature_location):
+        '''Update the months for the dict'''
 
         weeks_to_update = self.get_weeks_to_update(wk_no, 4)
-        for item in weeks_to_update:
-            self.update_week(xbin, ybin, item, yr, total_call, feature_location)
-
+        for new_week_number, year_offset in weeks_to_update:
+            self.update_week(xbin, ybin, new_week_number, yr-year_offset, total_call, feature_location)
 
     def update_year(self, xbin, ybin, wk_no, yr, total_call, feature_location):
+        '''Update the year for the dict'''
 
-        weeks_to_update = self.get_weeks_to_update(wk_no, 12)
+        weeks_to_update = self.get_weeks_to_update(wk_no, 51)
 
-        for item in weeks_to_update:
-            self.update_week(xbin, ybin, item, yr, total_call, feature_location)
-
+        for new_week_number, year_offset in weeks_to_update:
+            self.update_week(xbin, ybin, new_week_number, yr-year_offset, total_call, feature_location)
 
     def write_to_file(self):
         with open('update_feature_vector.csv', 'w') as mycsvfile:
@@ -244,18 +249,14 @@ class Feature_Vector:
 
 def main():
     fv = Feature_Vector(0)
-
-
     fv.initialize_feature_dict()
 
     csv_file = fv.load_file('countable_data.csv', 8)
     lookup_file = fv.load_file('../date_lookup_table.csv', 3)
     lookup_dict = fv.lookup_to_dict(lookup_file)
 
-
     fv.get_call_count(csv_file, lookup_dict)
     fv.write_to_file()
-
 
 
 if __name__ == "__main__":
