9b546c96e99361557a47b9c3087dcf787d6c506b
schachte <rschacht@asu.edu>
2016-11-21 14:34:50 -0700

    train_test update

--- python_scripts/training_testing_data.py
+++ python_scripts/training_testing_data.py
@@ -34,11 +34,15 @@ from cmath import sqrt
  16-31: radius 1 data
  32-47: radius 2 data
  48-63: radius 3 data
- 64-66: All-time data
- 67-70: Identifying data
- 71: Classification
+ 64-75: All-time data
+ 76-79: Identifying data
+ 80: Classification
 '''
 def get_feature_vectors(year, god_vector_this_week, god_vector_last_week, god_vector_2_weeks, god_vector_next_week, classifier_limit):
+    month_offset = 5
+    year_offset = 10
+    radii_offset = 18
+
     this_year_features = []
     year2 = year
     year1 = year
@@ -66,96 +70,108 @@ def get_feature_vectors(year, god_vector_this_week, god_vector_last_week, god_ve
 
 
     #Add month data to feature vector
-    this_year_features.append(god_vector_2_weeks[year2 + 6])         #last month, 2 weeks ago (most recent data)
-    this_year_features.append(god_vector_2_weeks[year2 + 6 + 1])     #last month, 2 weeks ago (1 year ago)
-    this_year_features.append(god_vector_2_weeks[year2 + 6 + 2])     #last month, 2 weeks ago (2 years ago)
-    this_year_features.append(god_vector_next_week[yearf + 6 + 1])   #last month, next week (1 year ago)
-    this_year_features.append(god_vector_next_week[yearf + 6 + 2])   #last month, next week (2 years ago)
+    this_year_features.append(god_vector_2_weeks[year2 + month_offset])         #last month, 2 weeks ago (most recent data)
+    this_year_features.append(god_vector_2_weeks[year2 + month_offset + 1])     #last month, 2 weeks ago (1 year ago)
+    this_year_features.append(god_vector_2_weeks[year2 + month_offset + 2])     #last month, 2 weeks ago (2 years ago)
+    this_year_features.append(god_vector_next_week[yearf + month_offset + 1])   #last month, next week (1 year ago)
+    this_year_features.append(god_vector_next_week[yearf + month_offset + 2])   #last month, next week (2 years ago)
 
     #Add Year data to feature vector
-    this_year_features.append(god_vector_2_weeks[year2 + 11])         #1 year as of 2 weeks ago (most recent data)
-    this_year_features.append(god_vector_this_week[year + 11 + 1])   #2 years ago as of this week
+    this_year_features.append(god_vector_2_weeks[year2 + year_offset])         #1 year as of 2 weeks ago (most recent data)
+    this_year_features.append(god_vector_this_week[year + year_offset + 1])   #2 years ago as of this week
 
 
     #Add radius 1 to feature vector
     #Add week data to feature vector
-    this_year_features.append(god_vector_2_weeks[year2 + 18])         #2 weeks ago (most recent data)
-    this_year_features.append(god_vector_2_weeks[year2 + 18 + 1])     #2 weeks ago (1 year ago)
-    this_year_features.append(god_vector_2_weeks[year2 + 18 + 2])     #2 weeks ago (2 years ago)
-    this_year_features.append(god_vector_last_week[year1 + 18 + 1])   #last week (1 year ago)
-    this_year_features.append(god_vector_last_week[year1 + 18 + 2])   #last week (2 years ago)
-    this_year_features.append(god_vector_this_week[year + 18 + 1])   #this week (1 year ago)
-    this_year_features.append(god_vector_this_week[year + 18 + 2])   #this week (2 years ago)
-    this_year_features.append(god_vector_next_week[yearf + 18 + 1])   #next week (1 year ago)
-    this_year_features.append(god_vector_next_week[yearf + 18 + 2])   #next week (2 years ago)
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset])         #2 weeks ago (most recent data)
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset + 1])     #2 weeks ago (1 year ago)
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset + 2])     #2 weeks ago (2 years ago)
+    this_year_features.append(god_vector_last_week[year1 + radii_offset + 1])   #last week (1 year ago)
+    this_year_features.append(god_vector_last_week[year1 + radii_offset + 2])   #last week (2 years ago)
+    this_year_features.append(god_vector_this_week[year + radii_offset + 1])   #this week (1 year ago)
+    this_year_features.append(god_vector_this_week[year + radii_offset + 2])   #this week (2 years ago)
+    this_year_features.append(god_vector_next_week[yearf + radii_offset + 1])   #next week (1 year ago)
+    this_year_features.append(god_vector_next_week[yearf + radii_offset + 2])   #next week (2 years ago)
 
 
     #Add month data to feature vector
-    this_year_features.append(god_vector_2_weeks[year2 + 18 + 6])         #last month, 2 weeks ago (most recent data)
-    this_year_features.append(god_vector_2_weeks[year2 + 18 + 6 + 1])     #last month, 2 weeks ago (1 year ago)
-    this_year_features.append(god_vector_2_weeks[year2 + 18 + 6 + 2])     #last month, 2 weeks ago (2 years ago)
-    this_year_features.append(god_vector_next_week[yearf + 18 + 6 + 1])   #last month, next week (1 year ago)
-    this_year_features.append(god_vector_next_week[yearf + 18 + 6 + 2])   #last month, next week (2 years ago)
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset + month_offset])         #last month, 2 weeks ago (most recent data)
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset + month_offset + 1])     #last month, 2 weeks ago (1 year ago)
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset + month_offset + 2])     #last month, 2 weeks ago (2 years ago)
+    this_year_features.append(god_vector_next_week[yearf + radii_offset + month_offset + 1])   #last month, next week (1 year ago)
+    this_year_features.append(god_vector_next_week[yearf + radii_offset + month_offset + 2])   #last month, next week (2 years ago)
 
     #Add Year data to feature vector
-    this_year_features.append(god_vector_2_weeks[year2 + 18 + 11])         #1 year as of 2 weeks ago (most recent data)
-    this_year_features.append(god_vector_this_week[year + 18 + 11 + 1])   #2 years ago as of this week
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset + year_offset])         #1 year as of 2 weeks ago (most recent data)
+    this_year_features.append(god_vector_this_week[year + radii_offset + year_offset + 1])   #2 years ago as of this week
 
 
     #Add radius 2 to feature vector
     #Add week data to feature vector
-    this_year_features.append(god_vector_2_weeks[year2 + 36])         #2 weeks ago (most recent data)
-    this_year_features.append(god_vector_2_weeks[year2 + 36 + 1])     #2 weeks ago (1 year ago)
-    this_year_features.append(god_vector_2_weeks[year2 + 36 + 2])     #2 weeks ago (2 years ago)
-    this_year_features.append(god_vector_last_week[year1 + 36 + 1])   #last week (1 year ago)
-    this_year_features.append(god_vector_last_week[year1 + 36 + 2])   #last week (2 years ago)
-    this_year_features.append(god_vector_this_week[year + 36 + 1])   #this week (1 year ago)
-    this_year_features.append(god_vector_this_week[year + 36 + 2])   #this week (2 years ago)
-    this_year_features.append(god_vector_next_week[yearf + 36 + 1])   #next week (1 year ago)
-    this_year_features.append(god_vector_next_week[yearf + 36 + 2])   #next week (2 years ago)
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset*2])         #2 weeks ago (most recent data)
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset*2 + 1])     #2 weeks ago (1 year ago)
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset*2 + 2])     #2 weeks ago (2 years ago)
+    this_year_features.append(god_vector_last_week[year1 + radii_offset*2 + 1])   #last week (1 year ago)
+    this_year_features.append(god_vector_last_week[year1 + radii_offset*2 + 2])   #last week (2 years ago)
+    this_year_features.append(god_vector_this_week[year + radii_offset*2 + 1])   #this week (1 year ago)
+    this_year_features.append(god_vector_this_week[year + radii_offset*2 + 2])   #this week (2 years ago)
+    this_year_features.append(god_vector_next_week[yearf + radii_offset*2 + 1])   #next week (1 year ago)
+    this_year_features.append(god_vector_next_week[yearf + radii_offset*2 + 2])   #next week (2 years ago)
 
 
     #Add month data to feature vector
-    this_year_features.append(god_vector_2_weeks[year2 + 36 + 6])         #last month, 2 weeks ago (most recent data)
-    this_year_features.append(god_vector_2_weeks[year2 + 36 + 6 + 1])     #last month, 2 weeks ago (1 year ago)
-    this_year_features.append(god_vector_2_weeks[year2 + 36 + 6 + 2])     #last month, 2 weeks ago (2 years ago)
-    this_year_features.append(god_vector_next_week[yearf + 36 + 6 + 1])   #last month, next week (1 year ago)
-    this_year_features.append(god_vector_next_week[yearf + 36 + 6 + 2])   #last month, next week (2 years ago)
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset*2 + month_offset])         #last month, 2 weeks ago (most recent data)
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset*2 + month_offset + 1])     #last month, 2 weeks ago (1 year ago)
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset*2 + month_offset + 2])     #last month, 2 weeks ago (2 years ago)
+    this_year_features.append(god_vector_next_week[yearf + radii_offset*2 + month_offset + 1])   #last month, next week (1 year ago)
+    this_year_features.append(god_vector_next_week[yearf + radii_offset*2 + month_offset + 2])   #last month, next week (2 years ago)
 
     #Add Year data to feature vector
-    this_year_features.append(god_vector_2_weeks[year2 + 36 + 11])         #1 year as of 2 weeks ago (most recent data)
-    this_year_features.append(god_vector_this_week[year + 36 + 11 + 1])   #2 years ago as of this week
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset*2 + year_offset])         #1 year as of 2 weeks ago (most recent data)
+    this_year_features.append(god_vector_this_week[year + radii_offset*2 + year_offset + 1])   #2 years ago as of this week
 
 
     #Add radius 3 to feature vector
     #Add week data to feature vector
-    this_year_features.append(god_vector_2_weeks[year2 + 36])         #2 weeks ago (most recent data)
-    this_year_features.append(god_vector_2_weeks[year2 + 36 + 1])     #2 weeks ago (1 year ago)
-    this_year_features.append(god_vector_2_weeks[year2 + 36 + 2])     #2 weeks ago (2 years ago)
-    this_year_features.append(god_vector_last_week[year1 + 36 + 1])   #last week (1 year ago)
-    this_year_features.append(god_vector_last_week[year1 + 36 + 2])   #last week (2 years ago)
-    this_year_features.append(god_vector_this_week[year + 36 + 1])   #this week (1 year ago)
-    this_year_features.append(god_vector_this_week[year + 36 + 2])   #this week (2 years ago)
-    this_year_features.append(god_vector_next_week[yearf + 36 + 1])   #next week (1 year ago)
-    this_year_features.append(god_vector_next_week[yearf + 36 + 2])   #next week (2 years ago)
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset*3])         #2 weeks ago (most recent data)
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset*3 + 1])     #2 weeks ago (1 year ago)
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset*3 + 2])     #2 weeks ago (2 years ago)
+    this_year_features.append(god_vector_last_week[year1 + radii_offset*3 + 1])   #last week (1 year ago)
+    this_year_features.append(god_vector_last_week[year1 + radii_offset*3 + 2])   #last week (2 years ago)
+    this_year_features.append(god_vector_this_week[year + radii_offset*3 + 1])   #this week (1 year ago)
+    this_year_features.append(god_vector_this_week[year + radii_offset*3 + 2])   #this week (2 years ago)
+    this_year_features.append(god_vector_next_week[yearf + radii_offset*3 + 1])   #next week (1 year ago)
+    this_year_features.append(god_vector_next_week[yearf + radii_offset*3 + 2])   #next week (2 years ago)
 
 
     #Add month data to feature vector
-    this_year_features.append(god_vector_2_weeks[year2 + 36 + 6])         #last month, 2 weeks ago (most recent data)
-    this_year_features.append(god_vector_2_weeks[year2 + 36 + 6 + 1])     #last month, 2 weeks ago (1 year ago)
-    this_year_features.append(god_vector_2_weeks[year2 + 36 + 6 + 2])     #last month, 2 weeks ago (2 years ago)
-    this_year_features.append(god_vector_next_week[yearf + 36 + 6 + 1])   #last month, next week (1 year ago)
-    this_year_features.append(god_vector_next_week[yearf + 36 + 6 + 2])   #last month, next week (2 years ago)
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset*3 + month_offset])         #last month, 2 weeks ago (most recent data)
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset*3 + month_offset + 1])     #last month, 2 weeks ago (1 year ago)
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset*3 + month_offset + 2])     #last month, 2 weeks ago (2 years ago)
+    this_year_features.append(god_vector_next_week[yearf + radii_offset*3 + month_offset + 1])   #last month, next week (1 year ago)
+    this_year_features.append(god_vector_next_week[yearf + radii_offset*3 + month_offset + 2])   #last month, next week (2 years ago)
 
     #Add Year data to feature vector
-    this_year_features.append(god_vector_2_weeks[year2 + 36 + 11])         #1 year as of 2 weeks ago (most recent data)
-    this_year_features.append(god_vector_this_week[year + 36 + 11 + 1])   #2 years ago as of this week
+    this_year_features.append(god_vector_2_weeks[year2 + radii_offset*3 + year_offset])         #1 year as of 2 weeks ago (most recent data)
+    this_year_features.append(god_vector_this_week[year + radii_offset*3 + year_offset + 1])   #2 years ago as of this week
 
 
     #Add all time to feature vector
-    this_year_features.append(god_vector_this_week[year + 15])     #2 weeks ago (most recent data)
-    this_year_features.append(god_vector_this_week[year + 16])     #2 weeks ago (1 year ago)
-    this_year_features.append(god_vector_this_week[year + 17])     #2 weeks ago (2 years ago)
+    this_year_features.append(god_vector_this_week[18])     #all weeks
+    this_year_features.append(god_vector_this_week[19])     #all months
+    this_year_features.append(god_vector_this_week[20])     #all time
+
+    this_year_features.append(god_vector_this_week[18 + radii_offset])     #all weeks radius 1
+    this_year_features.append(god_vector_this_week[19 + radii_offset])     #all months radius 1
+    this_year_features.append(god_vector_this_week[20 + radii_offset])     #all time radius 1
+
+    this_year_features.append(god_vector_this_week[18 + radii_offset*2])     #all weeks radius 2
+    this_year_features.append(god_vector_this_week[19 + radii_offset*2])     #all months radius 2
+    this_year_features.append(god_vector_this_week[20 + radii_offset*2])     #all time radius 2
+
+    this_year_features.append(god_vector_this_week[18 + radii_offset*3])     #all weeks radius 3
+    this_year_features.append(god_vector_this_week[19 + radii_offset*3])     #all months radius 3
+    this_year_features.append(god_vector_this_week[20 + radii_offset*3])     #all time radius 3
 
 
     #Add identifying data
@@ -196,7 +212,7 @@ def get_crime_mean(feature_vector):
 
 
     for x in feature_vector:
-        if feature_vector[x][18] != 0:
+        if feature_vector[x][20] != 0:
             total_sum += feature_vector[x][4] + feature_vector[x][5] + feature_vector[x][6]+ feature_vector[x][7]
             count += 4
             if feature_vector[x][2] <= 25:
@@ -216,11 +232,11 @@ def get_crime_standard_deviation(feature_vector, avg):
     count = 0
 
     for x in feature_vector:
-        if feature_vector[x][18] != 0:
+        if feature_vector[x][20] != 0:
             total_sum += (feature_vector[x][4]-avg)**2 + (feature_vector[x][5]-avg)**2 + (feature_vector[x][6]-avg)**2 + (feature_vector[x][7]-avg)**2
             count += 4
             if feature_vector[x][2] <= 25:
-                total_sum += (feature_vector[x][4]-avg)**2
+                total_sum += (feature_vector[x][3]-avg)**2
                 count += 1
 
 
@@ -243,8 +259,9 @@ def get_crime_stats(feature_vector):
 def write_features_to_file(feature_vector, classifier_limit):
     with open('test_train_data.csv', 'w') as mycsvfile:
         thedatawriter = csv.writer(mycsvfile)
+        counter = 0
         for row in feature_vector:
-            if feature_vector[row][18] != 0:
+            if feature_vector[row][20] != 0:
                 xbin = row[0]
                 ybin = row[1]
                 weeknum = row[2]
@@ -258,3 +275,29 @@ def write_features_to_file(feature_vector, classifier_limit):
                 god_vector_next_week = feature_vector[(xbin, ybin, (weeknum + 1) % 52,)]
 
                 year_2014 = get_feature_vectors(5, god_vector_this_week, god_vector_last_week, god_vector_2_weeks, god_vector_next_week, classifier_limit)
+                thedatawriter.writerow(year_2014)
+                year_2015 = get_feature_vectors(4, god_vector_this_week, god_vector_last_week, god_vector_2_weeks, god_vector_next_week, classifier_limit)
+                thedatawriter.writerow(year_2015)
+
+                if weeknum < 25:
+                    year_2016 = get_feature_vectors(3, god_vector_this_week, god_vector_last_week, god_vector_2_weeks, god_vector_next_week, classifier_limit)
+                    thedatawriter.writerow(year_2016)
+
+                if counter%10000 == 0:
+                    print counter/10000
+
+                counter += 1
+
+def main():
+
+    print("Test train")
+    feature_vector = open_the_file()
+    print 'File opened'
+    print 'Calculating training and testing data'
+    classifier_limit = get_crime_stats(feature_vector)
+
+    write_features_to_file(feature_vector, classifier_limit)
+
+
+if __name__ == '__main__':
+    main()
